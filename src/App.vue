882eaafdcfe8d5b34ae4b229406f5becc39961d1
igor <78038892+iterhim@users.noreply.github.com>
2022-06-04 21:41:55 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <!--  <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+  <h1>Todo list</h1>
+  <input style="cursor: pointer" v-model=message />
+  <button v-on:click="addTask(tasks)">Добавити таск</button>
+  <p v-for="(task, index) in tasks" v-bind:key="index">
+    <task :task="task" @remove="removeTask(index)" />
+  </p>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Task from './components/task.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Task
+  }, data() {
+    return {
+      message: 'Вивчити Vue.js!',
+      tasks: ["asd", 'asd', 'asd'],
+    }
+  },
+  methods: {
+    addTask: function () {
+      if (this.message) {
+        this.tasks.push(this.message)
+        this.message = ''
+        console.log(this.tasks)
+      }
+    },
+    removeTask: function (index) {
+      delete this.tasks[index];
+    },
+
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/task.vue
@@ -0,0 +1,27 @@
+<template>
+    {{ task }}
+    <button type="button" v-if="task" v-on:click="$emit('remove') ">done</button>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Task",
+  data() {
+    return {
+      text: 'asd'
+    }
+  },
+  props: {
+    task: String,
+  },
+
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
